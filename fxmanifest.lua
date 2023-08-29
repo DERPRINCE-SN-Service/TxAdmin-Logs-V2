09cbbe93f1c121398bdc8b5608cbb63e2e6d8072
DERPRINCE-SN-Service <140327731+DERPRINCE-SN-Service@users.noreply.github.com>
2023-08-29 20:56:56 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game 'gta5'
+author 'DER PRINCE/SN_Service.io'
+version '1.1.0'
+lua54 'yes'
+
+escrow_ignore {
+    'config.lua',
+}
+server_scripts {
+	'sv_txadmin.lua',
+	'config.lua'
+}
