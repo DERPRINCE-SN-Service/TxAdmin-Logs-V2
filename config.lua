cffc65cd038075d038318fc66259e2fb494679e2
DERPRINCE-SN-Service <140327731+DERPRINCE-SN-Service@users.noreply.github.com>
2023-08-29 20:59:06 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,16 @@
+
+--███████ ███    ██       ███████ ███████ ██████  ██    ██ ██  ██████ ███████
+--██      ████   ██       ██      ██      ██   ██ ██    ██ ██ ██      ██     
+--███████ ██ ██  ██ █████ ███████ █████   ██████  ██    ██ ██ ██      █████  
+--     ██ ██  ██ ██            ██ ██      ██   ██  ██  ██  ██ ██      ██     
+--███████ ██   ████       ███████ ███████ ██   ██   ████   ██  ██████ ███████
+
+--made by SN-Service https://discord.gg/GxY2vUGSrC
+
+Config = {}
+
+Config.txAdminWebhook = ''          -- Webhook here
+
+Config.Username = 'TxAdminLogs'     -- Bot Name 
+
+Config.FilterAnnouncements = true   -- Announce true/false
